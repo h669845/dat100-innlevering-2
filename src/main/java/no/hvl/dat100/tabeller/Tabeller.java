d683931c71418f47580bc895561211635cd6fe88
h669845 <patrik.thormodsen@gmail.com>
2024-09-21 17:36:49 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -5,38 +5,84 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
+		System.out.print("[ ");
+		for (int i = 0; i < tabell.length; i++) {
+			System.out.print(tabell[i] + " ");
+		}
+		System.out.print("]");
+		System.out.println("");
 		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
 
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		String output = "[";
+		
+		for (int i = 0; i < tabell.length; i++) {
+			
+			
+			if (i == tabell.length - 1) {
+				output += tabell[i];
+			} else {
+				output += tabell[i] + ",";
+			}
+		}
+
+		output += "]";
+		
+		System.out.println(output);
+		return output;
+
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
+		int sum = 0;
+		
+		for (int i = 0; i < tabell.length; i++) {
+			sum += tabell[i];
+		}
+		
+		System.out.println(sum);
+		return sum;
+
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
+		boolean finnes = false;
+		
+		for (int i = 0; i<tabell.length; i++) {
+			if (tabell[i] == tall) {
+				finnes = true;
+			}
+		}
+		
+		System.out.println(finnes);
+		return finnes;
+		
 
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
+		int verdi = -1;
+		
+		for (int i = 0; i<tabell.length;i++) {
+			if (tabell[i] == tall) {
+				verdi = i;
+				break;
+			} 
+		}
+		
+		System.out.println(verdi);
+		return verdi;
+		
 	}
 
 	// f)
